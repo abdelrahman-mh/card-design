576320de0bca92e4dbc800fa1e9a80407977c292
abdelrahman-mh <abdelrahman.mh3@gmail.com>
2024-03-28 02:43:27 +0200

    base build

---
+++ README.md
@@ -0,0 +1,27 @@
+### Builds with 100% with Tailwind Css;
+
+## For test it :point_right:
+
+Clone the app:
+
+```shell
+git clone <name>
+
+# after go to the app dire
+cd <name>
+```
+
+Install dependencies:
+
+```shell
+npm install
+```
+
+Run the app:
+
+```shell
+npm start
+# 👍
+```
+
+![screenshot](./docs/screenshot.jpg)
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      green: "hsl(75, 94%, 57%)",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 20%)",
+      "dark-grey": "hsl(0, 0%, 12%)",
+      "off-black": "hsl(0, 0%, 8%)",
+    },
   },
   plugins: [],
-}
-
+};
